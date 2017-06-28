de0a232a2883f48efcbd7be8b96019b3f3f67c1c
kenzocosmos <kenzocosmos@gmail.com>
2017-06-28 15:40:02 +0000

    Completed authentication with virtual attrs for user model

--- app/controllers/concerns/devise_whitelist.rb
+++ app/controllers/concerns/devise_whitelist.rb
@@ -3,10 +3,10 @@ module DeviseWhitelist
   
   included do
        before_action :configure_permitted_parameters, if: :devise_controller?
- 
   end
+  
   def configure_permitted_parameters
      devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) 
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
